8be2270c7629d54f2d5b93b536ea6c2889ea6933
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2026-05-19 23:31:03 +0200

    Add shadcn ui

---
+++ components/web/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Navbar() {
+  return (
+    <nav>
+        <div>
+            <div>
+                Blog <span>Pro</span>
+            </div>
+        </div>
+    </nav>
+  )
+}

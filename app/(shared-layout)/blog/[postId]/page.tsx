cd2e73c1b296b1d180f0aa80f46a8ee15dd3071c
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2026-08-12 22:06:05 +0200

    Add metadata

--- app/(shared-layout)/blog/[postId]/page.tsx
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -1,6 +1,34 @@
 import { Suspense } from 'react'
 import { PostContent } from './post-content'
 import { SingleBlogSkeleton } from './single-blog-skeleton'
+import { Metadata } from 'next'
+import { fetchQuery } from 'convex/nextjs'
+import { Id } from '@/convex/_generated/dataModel'
+import { api } from '@/convex/_generated/api'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ postId: string }>
+}): Promise<Metadata> {
+  const { postId } = await params
+  const post = await fetchQuery(api.queries.posts.getPostById, {
+    postId: postId as Id<"posts">,
+  })
+
+  if (!post) return {}
+
+  return {
+    title: post.title,
+    description: post.content.slice(0, 150),
+    openGraph: {
+      title: post.title,
+      description: post.content.slice(0, 150),
+      images: post.imageUrl ? [post.imageUrl] : [],
+      type: "article",
+    },
+  }
+}
 
 export default async function SingleBlogPage({
   params,
